356af4185fc6d053f1dc8875266184443fc5da69
Curtis Parfitt-Ford <curtis@mashedkeyboard.me>
2019-02-21 17:37:17 +0000

    Made CSS and JS optional
    So they can be included in stylesheets or custom JS files elsewhere at user choice

--- lib/formtastic/ace_editor.rb
+++ lib/formtastic/ace_editor.rb
@@ -7,39 +7,48 @@ module Formtastic
       def to_html
         input_wrapping do
           label_html << builder.text_area(method, input_html_options) <<
-          (<<-JS).html_safe
+          (<<-HTML).html_safe
           <div id="#{dom_id}-container" class="ace_editor_container"><div id="#{dom_id}-editor"></div></div>
-          <style type="text/css">
-            ##{dom_id} { display: none; }
-            ##{dom_id}-container {
-              height: #{height};
-              position: relative;
-            }
-            ##{dom_id}-editor {
-              position: absolute;
-              top: 0;
-              bottom: 0;
-              left: #{left};
-              right: 0;
-            }
-          </style>
-          <script type="text/javascript">
-            (function() {
-              var editor = ace.edit('#{dom_id}-editor');
-              editor.setValue(document.getElementById('#{dom_id}').value);
-              editor.getSession().setUseWorker(false);
-              editor.setTheme('ace/theme/#{theme}');
-              editor.getSession().setMode('ace/mode/#{mode}');
-              editor.clearSelection();
-              editor.getSession().setUseSoftTabs(true);
-              editor.getSession().setTabSize(2);
-              editor.getSession().on('change', function(e) {
-                document.getElementById('#{dom_id}').value = editor.getValue();
-              });
-            })();
-          </script>
-          JS
+          HTML
+          if use_css
+            label_html << (<<-CSS).html_safe
+            <style type="text/css">
+              ##{dom_id} { display: none; }
+              ##{dom_id}-container {
+                height: #{height};
+                position: relative;
+              }
+              ##{dom_id}-editor {
+                position: absolute;
+                top: 0;
+                bottom: 0;
+                left: #{left};
+                right: 0;
+              }
+            </style>
+            CSS
+          end
+          if use_js
+            label_html << (<<-JS).html_safe
+            <script type="text/javascript">
+              (function() {
+                var editor = ace.edit('#{dom_id}-editor');
+                editor.setValue(document.getElementById('#{dom_id}').value);
+                editor.getSession().setUseWorker(false);
+                editor.setTheme('ace/theme/#{theme}');
+                editor.getSession().setMode('ace/mode/#{mode}');
+                editor.clearSelection();
+                editor.getSession().setUseSoftTabs(true);
+                editor.getSession().setTabSize(2);
+                editor.getSession().on('change', function(e) {
+                  document.getElementById('#{dom_id}').value = editor.getValue();
+                });
+              })();
+            </script>
+            JS
+          end
         end
+        label_html
       end
 
       def theme
@@ -56,6 +65,14 @@ module Formtastic
     
       def left
         options[:left] || '0'
+      end
+     
+      def use_js
+        options[:use_js] || true
+      end
+    
+      def use_css
+        options[:use_css] || true
       end
     end
   end
